82dc417d0386734a57fc24e70acf66a713506c9a
tintoverano <gtintoverano@gmail.com>
2015-03-14 15:08:29 +0100

    categories checkbox replaced with map-icons

--- client/around.js
+++ client/around.js
@@ -3,3 +3,12 @@ Template.around.helpers ({
     return Session.get ("aroundMe");
   }
 });
+
+Template.around.helpers ({
+  setIcon: function (aCategory) {
+    if (aCategory == undefined)
+      return "";
+    var category = _.findWhere (mapIcons, {category: aCategory});
+    return category.icon;
+  }
+});
--- client/categories.js
+++ client/categories.js
@@ -1,10 +1,33 @@
+mapIcons = [
+  {category: "irodatechnika irodaszer ", icon: "map-icon-school"},
+  {category: "éttermek kávézók ", icon: "map-icon-restaurant"},
+  {category: "oktatás ", icon: "map-icon-library"},
+  {category: "borszaküzletek pincék ", icon: "map-icon-liquor-store"},
+  {category: "hotelek panziók ", icon: "map-icon-lodging"},
+  {category: "sport szabadidő ", icon: "map-icon-physiotherapist"},
+  {category: "egészség ", icon: "map-icon-spa"},
+  {category: "hvg-kedvezmények ", icon: "map-icon-bank"},
+  {category: "utazás ", icon: "map-icon-travel-agency"},
+  {category: "bútor lakberendezés ", icon: "map-icon-furniture-store"},
+  {category: "könyv zenemű ", icon: "map-icon-book-store"}
+];
+
 Template.categories.helpers ({
   myCategories: function () {
     var categories = [];
-    Session.get ("aroundMe").forEach (function (place) {
-      if (!_.contains (categories, place.kategoria))
-        categories.push (place.kategoria);
-    });
+    var aroundMe = Session.get ("aroundMe");
+    if (aroundMe)
+      aroundMe.forEach (function (place) {
+        if (!_.contains (categories, place.kategoria))
+          categories.push (place.kategoria);
+      });
     return categories;
+  },
+
+  setIcon: function (aCategory) {
+    if (aCategory == undefined)
+      return "";
+    var category = _.findWhere (mapIcons, {category: aCategory});
+    return category.icon;
   }
 });
